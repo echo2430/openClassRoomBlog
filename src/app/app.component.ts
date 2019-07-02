ecdbbfca6b320b22103be1377849bee17e42d2bc
echo2430 <echo2430@gmail.com>
2019-07-02 15:58:49 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  posts = [{
+      title: "Mon premier post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis",
+      loveIts: 0
+    }, {
+      title: "Mon deuxieme post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis",
+      loveIts: 0,
+    }, {
+      title: "Encore un post",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+      loveIts: 0,
+    }]
+
+  title = 'openClassRoomBlog';
+}
